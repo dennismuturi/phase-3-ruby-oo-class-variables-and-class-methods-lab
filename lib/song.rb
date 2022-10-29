c18e0b3fcd1a5dddd5ff85584d06d60b7584a6ab
dennismuturi <dennismuturi1@gmail.com>
2022-10-29 12:32:07 +0300

    Completed assignment

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+    attr_accessor :name
+    attr_accessor :artist
+    attr_accessor :genre
+
+    @@count = 0
+    @@genres=[]
+    @@artists=[]
+
+    def initialize(name,artist,genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        
+        @@count +=1
+        @@genres << self.genre.downcase
+        @@artists << self.artist
+    end
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        genres=[]
+        @@genres.each do |genre| 
+            genres << genre if !genres.include?(genre)
+        end
+        genres
+    end
+
+    def self.artists
+        artists=[]
+        @@artists.each do |artist| 
+            artists << artist if !artists.include?(artist)
+        end
+        artists
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
+
+win = Song.new("All I do is win","dj khaled","rap")
+deuces = Song.new("Deuces","Chris Brown","rnb")
+amapiano =Song.new("Amakinki","Major League Djz","amapiano")
+congragulation = Song.new("Congragulation","Quavo","rap")
+liveyourlife = Song.new("Live Your Life","Rihanna","RAP")
+russian = Song.new("russian Roullette","rihanna","rnb")
